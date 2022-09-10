ff1bd27345a560e2468b9ebb8afd92198fdf5eaf
Alfredo Rueda <alfredoru@gmail.com>
2022-09-10 02:36:04 +0200

    Transactional

--- src/test/java/com/ironhack/authors/repository/AuthorRepositoryTest.java
+++ src/test/java/com/ironhack/authors/repository/AuthorRepositoryTest.java
@@ -12,12 +12,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@Transactional
 class AuthorRepositoryTest {
 
     @Autowired
